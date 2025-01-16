d9277bc8c5357f057e06d5ede9bdabe8e3ad1a7b
Trushar2411 <trushar.ghanekar2411@gmail.com>
2025-01-16 19:36:03 +0100

    made changes in weather

--- weather_forecasting.pl
+++ weather_forecasting.pl
@@ -7,8 +7,3 @@ will_rain(Date) :-
     CloudCover > 0.75,       % High cloud cover
     Precipitation > 0.0.    % Some precipitation is expected
 
-% Rule to suggest it will not rain
-will_not_rain(Date) :-
-    weather(Date, _, _, CloudCover, Precipitation),
-    (CloudCover =< 0.75;    % Low cloud cover
-     Precipitation =< 0.0). % No precipitation expected
