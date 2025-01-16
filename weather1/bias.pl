88406b87f1c4cd24169b27dbd5e16ebb767eb2a4
moujtahid21 <abdelbasset.moujtahid@smail.th-koeln.de>
2025-01-16 20:44:57 +0100

    Minor changes for better compiling on windows

--- weather1/bias.pl
+++ weather1/bias.pl
@@ -1,6 +1,3 @@
-% Include weather facts
-:- consult('weather_facts.pl').
-
 % Head predicates
 head_pred(will_rain, 1).
 
@@ -8,17 +5,18 @@ head_pred(will_rain, 1).
 body_pred(weather, 5).
 
 % Type declarations
-type(will_rain, (date,)).
+type(will_rain, (date)).
 type(weather, (date, month, temperature, cloud_cover, precipitation)).
 
+% Direction declarations
+% Specify how arguments are used in the predicates
+direction(will_rain, (in,)).
+direction(weather, (in, in, in, in, in)).
+
 % Constraints
+
 % Ensure each clause uses exactly one variable of type `date`
 :-
     clause(C),
     #count{V : var_type(C, V, date)} != 1.
 
-% Additional constraints
-% Ensure precipitation and cloud cover are numeric
-:-
-    body_literal(C, weather(_, _, _, CloudCover, Precipitation)),
-    (CloudCover < 0.0; CloudCover > 1.0; Precipitation < 0.0).
