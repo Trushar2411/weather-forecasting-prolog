f31051084e8c141019dafe0189227b4180e13a92
Trushar2411 <trushar.ghanekar2411@gmail.com>
2025-01-17 16:20:22 +0100

    change popper files

--- weather1 copy/bk.pl
+++ weather1 copy/bk.pl
@@ -1,15 +1,43 @@
-% Suppress warnings about discontiguous predicates
-:- discontiguous pos/1.
-:- discontiguous neg/1.
+% Weather facts
+weather(20000101, 1, 4.2, 0.92, 0.22).
+weather(20000102, 1, 6.5, 0.87, 0.0).
+weather(20000103, 1, 7.7, 0.78, 0.28).
+weather(20000104, 1, 7.8, 0.87, 0.97).
+weather(20000105, 1, 5.2, 0.8, 0.0).
+weather(20000106, 1, 7.6, 0.83, 0.18).
+weather(20000107, 1, 6.6, 0.79, 0.0).
+weather(20000108, 1, 6.6, 0.85, 0.12).
+weather(20000109, 1, 2.5, 0.94, 0.0).
+weather(20000110, 1, 1.1, 0.96, 0.0).
+weather(20000111, 1, 0.7, 0.86, 0.0).
+weather(20000112, 1, 2.1, 0.79, 0.0).
+weather(20000113, 1, 1.3, 0.73, 0.02).
+weather(20000114, 1, 3.3, 0.85, 0.0).
+weather(20000115, 1, 2.5, 0.77, 0.0).
+weather(20000116, 1, 4.2, 0.93, 0.23).
+weather(20000117, 1, 6.2, 0.9, 0.13).
+weather(20000118, 1, 5.9, 0.78, 0.1).
+weather(20000119, 1, 4.1, 0.79, 0.02).
+weather(20000120, 1, 4.0, 0.87, 0.08).
 
-% Include weather facts
-:- consult('weather_facts.pl').
+% Extract cloud cover
+cloud_cover(Date, A) :-
+    weather(Date, _, _, A, _).
 
-% Declare weather predicate
-weather/5.
+% Rule for high cloud cover
+cloud_cover_high(Date) :-
+    cloud_cover(Date, CloudCover),
+    CloudCover > 0.75.
 
-% Include existing rule for testing
-will_rain(Date) :-
-    weather(Date, _, _, CloudCover, Precipitation),
-    CloudCover > 0.75,
-    Precipitation > 0.0.
+% Extract precipitation
+precipitation(Date, B) :-
+    weather(Date, _, _, _, B).
+
+% Rule for high precipitation
+precipitation_high(Date) :-
+    precipitation(Date, Precipitation),
+    Precipitation > 0.1.
+
+% Extract temperature
+temperature(Date, C) :-
+    weather(Date, C, _, _, _).
--- weather1 copy/exs.pl
+++ weather1 copy/exs.pl
@@ -1,7 +1,45 @@
-% Positive examples
-pos(will_rain(20000101)).  % Example with precipitation
-pos(will_rain(20000104)).  % Another rainy day
+% Positive examples for cloud_cover_high
+pos(cloud_cover_high(20000101)).
+pos(cloud_cover_high(20000102)).
+pos(cloud_cover_high(20000103)).
+pos(cloud_cover_high(20000104)).
+pos(cloud_cover_high(20000105)).
+pos(cloud_cover_high(20000106)).
+pos(cloud_cover_high(20000107)).
+pos(cloud_cover_high(20000108)).
+pos(cloud_cover_high(20000109)).
+pos(cloud_cover_high(20000110)).
+pos(cloud_cover_high(20000111)).
+pos(cloud_cover_high(20000114)).
+pos(cloud_cover_high(20000116)).
+pos(cloud_cover_high(20000117)).
+pos(cloud_cover_high(20000120)).
 
-% Negative examples
-neg(will_rain(20000102)).  % No precipitation
-neg(will_rain(20000105)).  % Another dry day
+% Negative examples for cloud_cover_high
+neg(cloud_cover_high(20000113)).
+neg(cloud_cover_high(20000118)).
+neg(cloud_cover_high(20000119)).
+
+% Positive examples for precipitation_high
+pos(precipitation_high(20000101)).
+pos(precipitation_high(20000103)).
+pos(precipitation_high(20000104)).
+pos(precipitation_high(20000106)).
+pos(precipitation_high(20000108)).
+pos(precipitation_high(20000116)).
+pos(precipitation_high(20000117)).
+
+% Negative examples for precipitation_high
+neg(precipitation_high(20000102)).
+neg(precipitation_high(20000105)).
+neg(precipitation_high(20000107)).
+neg(precipitation_high(20000109)).
+neg(precipitation_high(20000110)).
+neg(precipitation_high(20000111)).
+neg(precipitation_high(20000112)).
+neg(precipitation_high(20000113)).
+neg(precipitation_high(20000114)).
+neg(precipitation_high(20000115)).
+neg(precipitation_high(20000118)).
+neg(precipitation_high(20000119)).
+neg(precipitation_high(20000120)).
